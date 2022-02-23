7ca826fe390a46b2eecf5c09651dca379d585854
jaiodoo <47434915+jaiodoo@users.noreply.github.com>
2022-02-23 16:32:55 -0500

    Actualizacion del hook

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM --platform=linux/amd64 python:3
 LABEL "maintainer" = "José Angel Inda Herrera <jose.inda09947@gmail.com>"
 RUN pip install --upgrade pip
-RUN pip install flake8 mypy isort black
+RUN pip install flake8
 RUN pip install --upgrade git+https://github.com/oca/pylint-odoo.git
 COPY entrypoint.sh /
 COPY .pylintrc /
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,39 +4,51 @@
 # $1: python-root
 # $2: flake8-flags
 # $3: skip-flake8
-# $4: skip-black
+
 
 cd "$1"
+submodules=$(git submodule foreach --quiet 'echo $sm_path')
+myarray=($submodules)
+
 for dir in $(ls -1)
  do
-  if [[ $dir == *"."* ]]
+  if printf '%s\n' "${myarray[@]}" | grep -Fxq "$dir";
    then
     continue
   fi
-   PYLINT_ERRORS+=$(python3 -m pylint --rcfile=/.pylintrc --load-plugins=pylint_odoo $2  "$dir")
-   exit_code=$?
+ if [[ $dir == *"."* ]]
+   then
+    continue
+  fi
+  if [[ $dir == *".orig" ]]
+   then
+    continue
+  fi
+  if [[ $dir == *".md" ]]
+   then
+    continue
+  fi
+  if [[ $dir == *".rst" ]]
+   then
+    continue
+  fi
+  # Pylint-odoo
+  PYLINT_ERRORS+=$(python3 -m pylint --rcfile=/.pylintrc  --load-plugins=pylint_odoo $2 "$dir")
+  exit_code=$?
+  
+  # Flake8
+  if [ "$3" = '' ]; then
+    FLAKE8_ERRORS+=$(python3 -m flake8 $2 "$dir")
+    if [ "$exit_code" != "0" ]; then
+      exit_code=$exit_code
+    else
+      exit_code=$?
+    fi
+  fi
 done
+
 if [ "$exit_code" != "0" ]; then
-  printf "\npylint-odoo errors:\n-----------------\n%s\n-----------------\n" "$PYLINT_ERRORS"
+  printf "\ flake8 errors:\n-----------------\n%s\n-----------------\n" "$FLAKE8_ERRORS"
+  printf "\ pylint errors:\n-----------------\n%s\n-----------------\n" "$PYLINT_ERRORS"
   exit $exit_code
-fi
-
-if [ "$3" = '' ]; then
-  FLAKE8_ERRORS=$(python3 -m flake8 $2 "$1")
-  exit_code=$?
-
-  if [ "$exit_code" != "0" ]; then
-    printf "\nflake8 errors:\n-----------------\n%s\n-----------------\n" "$FLAKE8_ERRORS"
-    exit $exit_code
-  fi
-fi
-
-if [ "$4" = '' ]; then
-  BLACK_ERRORS=$(python3 -m black --line-length 80 "$1" --check)
-  exit_code=$?
-
-  if [ "$exit_code" != "0" ]; then
-    printf "\black errors:\n-----------------\n%s\n-----------------\n" "$BLACK_ERRORS"
-    exit $exit_code
-  fi
 fi
