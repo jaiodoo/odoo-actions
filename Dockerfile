1ea5055e0b8f166cb54fbeaeaabd179840958f57
jaiodoo <47434915+jaiodoo@users.noreply.github.com>
2022-02-23 12:19:26 -0500

    Version1.2.2

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM python:3
+FROM --platform=linux/amd64 python:3
+LABEL "maintainer" = "José Angel Inda Herrera <jose.inda09947@gmail.com>"
 RUN pip install --upgrade pip
-RUN pip install flake8 mypy isort
+RUN pip install flake8 mypy isort black
 RUN pip install --upgrade git+https://github.com/oca/pylint-odoo.git
 COPY entrypoint.sh /
 COPY .pylintrc /
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 # $1: python-root
 # $2: flake8-flags
 # $3: skip-flake8
+# $4: skip-black
 
 cd "$1"
 for dir in $(ls -1)
@@ -12,20 +13,30 @@ for dir in $(ls -1)
    then
     continue
   fi
-   PYLINT_ERRORS+=$(python3 -m pylint --rcfile=/.pylintrc --load-plugins=pylint_odoo -d all -e odoolint "$dir")
+   PYLINT_ERRORS+=$(python3 -m pylint --rcfile=/.pylintrc --load-plugins=pylint_odoo $2  "$dir")
    exit_code=$?
 done
 if [ "$exit_code" != "0" ]; then
-      printf "\npylint-odoo errors:\n-----------------\n%s\n-----------------\n" "$PYLINT_ERRORS"
-      exit $exit_code
-    fi
+  printf "\npylint-odoo errors:\n-----------------\n%s\n-----------------\n" "$PYLINT_ERRORS"
+  exit $exit_code
+fi
 
-if [ "$5" = false ]; then
+if [ "$3" = '' ]; then
   FLAKE8_ERRORS=$(python3 -m flake8 $2 "$1")
   exit_code=$?
 
   if [ "$exit_code" != "0" ]; then
     printf "\nflake8 errors:\n-----------------\n%s\n-----------------\n" "$FLAKE8_ERRORS"
+    exit $exit_code
+  fi
+fi
+
+if [ "$4" = '' ]; then
+  BLACK_ERRORS=$(python3 -m black --line-length 80 "$1" --check)
+  exit_code=$?
+
+  if [ "$exit_code" != "0" ]; then
+    printf "\black errors:\n-----------------\n%s\n-----------------\n" "$BLACK_ERRORS"
     exit $exit_code
   fi
 fi
